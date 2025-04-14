0f947e97078a48b9a4580f155abdf902359c34ca
Ross Tsachev <ross102t@gmail.com>
2025-04-14 09:37:36 +0300

    Add Cucumber feature, steps and runner

---
+++ src/test/java/org/opencartqa/steps/ShoppingCartSteps.java
@@ -0,0 +1,57 @@
+package org.opencartqa.steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import static org.junit.Assert.assertTrue;
+
+public class ShoppingCartSteps {
+    @Given("user is on home page")
+    public void user_is_on_home_page() {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+
+    @When("user adds item to shopping cart")
+    public void user_adds_item_to_shopping_cart() {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+
+    @When("clicks on the shopping cart icon")
+    public void clicks_on_the_shopping_cart_icon() {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+
+    @Then("user is navigated to shopping cart page")
+    public void user_is_navigated_to_shopping_cart_page() {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+
+    @Then("the added item is there")
+    public void the_added_item_is_there() {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+
+    @When("user selects shipping destination")
+    public void user_selects_shipping_destination() {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+
+    @When("chooses a rate available for that regioni")
+    public void chooses_a_rate_available_for_that_regioni() {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+
+    @Then("the shipping rate is added to the amount of the order")
+    public void the_shipping_rate_is_added_to_the_amount_of_the_order() {
+        // Write code here that turns the phrase above into concrete actions
+        assertTrue(true);
+    }
+}
---
+++ src/test/resources/org/opencartqa/features/ShoppingCartFeature.feature
@@ -0,0 +1,11 @@
+Feature: Shopping Cart
+
+  Scenario: Shipping is added to order
+    Given user is on home page
+    When user adds item to shopping cart
+    And clicks on the shopping cart icon
+    Then user is navigated to shopping cart page
+    And the added item is there
+    When user selects shipping destination
+    And chooses a rate available for that regioni
+    Then the shipping rate is added to the amount of the order
